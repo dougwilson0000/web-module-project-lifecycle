963bcbae68cd7c1190e25d930824735240fad43b
Douglas Wilson <dougwilson0000@gmail.com>
2022-01-11 22:11:33 -0500

    finished, need to add styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import User from './components/User';
 import FollowerList from './components/FollowerList';
+import axios from 'axios';
 
 class App extends React.Component {
 
@@ -23,11 +24,52 @@ class App extends React.Component {
     ] 
 
   }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then(resp=> {
+      this.setState({
+       ...this.state,
+        user: resp.data
+      });
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.user !== prevState.user) {
+      axios.get(`https://api.github.com/users/${this.state.currentUser}/followers`)
+    .then(resp=> {
+      this.setState({
+       ...this.state,
+        followers: resp.data
+      });
+    })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      currentUser: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then(resp=> {
+      this.setState({
+       ...this.state,
+        user: resp.data
+      });
+    })
+  }
+
   render() {
     return(<div>
       <h1>GITHUB INFO</h1>
-      <form>
-        <input defaultValue='Github Value' />
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} defaultValue='Github Value' />
         <button>Search</button>
 
       </form>
